d4a6995c77659f19a04491c3c8a3f82c45708e8d
Ernest-Opara <ernest.ebuka@gmail.com>
2020-06-03 00:21:00 +0100

    added my script

---
+++ scripts/ernest.py
@@ -0,0 +1,10 @@
+fullname = "Chukwuebuka Ernest-Opara"
+hng_id = "HNG-04557"
+language = "Python"
+
+def introduction():
+
+    result = ("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(fullname,hng_id,language))
+    print(result)
+
+introduction()
