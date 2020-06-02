0241c23f8c001fdd014ac1cd2ef30877fb73a01d
omichaels <31506153+blaknight@users.noreply.github.com>
2020-06-03 06:35:58 +0800

    Update michael_o.py

--- scripts/michael_o.py
+++ scripts/michael_o.py
@@ -1,14 +1,3 @@
-#import json
-
-#HNGIntro_string = '{"Hello World, this is Michael N.N Oku with HNGi7 ID HNG-03221 using PYTHON for stage2 task"}'
-
-# Getting dictionary
-#HNGIntro_dict = json.loads(HNGIntro_string)
-
-#print(json.dumps(HNGIntro_dict))
-
-#print("Hello World, this is Michael N.N Oku with HNGi7 ID HNG-03221 using PYTHON for stage2 task")
-
 def my_function():
   print("Hello World, this is Michael N.N Oku with HNGi7 ID HNG-03221 using PYTHON for stage2 task")
 
