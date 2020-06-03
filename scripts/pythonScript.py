cb0cf323e2e9720f1637dce451a6eff32c5ad2d3
David Adigwu <davidadigwu@gmail.com>
2020-06-03 01:55:32 +0100

    I added a script written with Python

---
+++ scripts/pythonScript.py
@@ -0,0 +1,7 @@
+fullName = "Adigwu David Uche";
+ID = "HNG-00567";
+language = "Python";
+
+result = 'Hello World, this is {0}  with HNGi7 ID {1} using {2} for stage2 task'.format(fullName, ID, language)
+
+print(result)
