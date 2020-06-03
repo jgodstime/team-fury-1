4935a947804d8d1d222fd4b55404cbbab54a30e3
David Adigwu <davidadigwu@gmail.com>
2020-06-03 01:54:25 +0100

    I added a script written with Javascript

---
+++ scripts/jsScript.js
@@ -0,0 +1,6 @@
+<script>
+var fullName = "Adigwu David Uche";
+var ID = "HNG-00567";
+var language = "JavaScript";
+document.write("Hello World, this is " + fullName + " with HNGi7 ID " + ID + " using " + language + " for stage2 task");
+</script>
