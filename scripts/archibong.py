e2371f0e17e0b41f0b3c4409cb04d67e5bc1b9c5
Arch997 <archibongc4@gmail.com>
2020-06-04 00:04:31 +0100

    Minor edit

--- scripts/archibong.py
+++ scripts/archibong.py
@@ -1,6 +1,4 @@
 
-
-
 class Task:
 	def __init__(self, name, id, language):
 		self.name = name
@@ -8,12 +6,10 @@ class Task:
 		self.id = id
 	
 	def text(self):
-		 print (f'Hello World, this is {self.name}, with HNGi7 ID ({self.id}) using {self.language} for Stage 2 task')
+		 print (f'Hello World, this is {self.name}, with HNGi7 ID {self.id} using {self.language} for Stage 2 task')
 
 email = 'archibongc4@gmail.com'
 
 t = Task("Charles Effiom", 'HNGi7-01950', "Python")
 t.text()
-
-
 
