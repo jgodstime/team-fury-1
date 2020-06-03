c11ca1d47fd7c137deba8d356dcc250688e95e6c
payn33 <amugenp2@gmail.com>
2020-06-04 00:20:24 +0100

    adds p_yn3.js file

---
+++ scripts/p_yn3.js
@@ -0,0 +1,11 @@
+var displayText = {
+  name:"Peter Amugen",
+  id:"HNGi-01447",
+  language:"javascript",
+  email:"amugenp@gmail.com"
+};
+
+console.log("Hello world, this is " + displayText.name + " with HNGi id " + displayText.id + " using " + displayText.language + " for stage 2 task.");
+
+var myJSON = JSON.stringify(displayText);
+console.log(myJSON);
