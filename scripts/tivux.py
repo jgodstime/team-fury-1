a4de3bae797562f485ca25c2664d283b1508ce9a
An Vu <anvutien99@gmail.com>
2020-06-03 10:44:52 +0100

    add my script for Stage 2 task

---
+++ scripts/tivux.py
@@ -0,0 +1,7 @@
+def print_text():
+    text = 'Hello World, this is {full_name} with HNGi7 ID {ID} using {language} for stage 2 task'
+
+    print(text.format(full_name="An Vu", ID="HNG-01042", language="Python"))
+
+
+print_text()
