e403b7c24faa5120e95ada9f9d2b7b46e653c548
Arch997 <archibongc4@gmail.com>
2020-06-03 17:05:08 +0100

    Added an email variable upon further instruction

--- scripts/archibong.py
+++ scripts/archibong.py
@@ -1,4 +1,6 @@
 
+
+
 class Task:
 	def __init__(self, name, id, language):
 		self.name = name
@@ -6,8 +8,12 @@ class Task:
 		self.id = id
 	
 	def text(self):
-		 print(f'Hello World, this is {self.name}, with HNGi7 ID ({self.id}) using {self.language} for Stage 2 task')
+		 print (f'Hello World, this is {self.name}, with HNGi7 ID ({self.id}) using {self.language} for Stage 2 task')
 
-t = Task('Charles Effiom', 'HNGi7-01950', 'Python')
+email = 'archibongc4@gmail.com'
+
+t = Task("Charles Effiom", 'HNGi7-01950', "Python")
 t.text()
+
+
 
