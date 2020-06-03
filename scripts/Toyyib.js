ff6428fed73e42cb71c48613d4c3eb799aec01ad
Toyyib06 <abdulghoniyhtoyyib@gmail.com>
2020-06-03 14:06:13 +0100

    first commit

--- scripts/Toyyib.js
+++ scripts/Toyyib.js
@@ -9,7 +9,7 @@ var myDetails = {
     
  
  function fullDetails () {
-    return "Hello World, this is " + myDetails.surname + " " + myDetails.lastname  + " with email address "  + " " + myDetails.email +
+    return "Hello World, this is " + myDetails.surname + " " + myDetails.lastname  + " with email address "  + myDetails.email +
     " and HNGi7 ID " + myDetails.hng7Id + " using " + myDetails.language +
     " for stage 2 task."
 
