c129dbd45ec905d1dea1896e19bedf48bc805808
Toyyib06 <abdulghoniyhtoyyib@gmail.com>
2020-06-03 12:30:00 +0100

    initial commit

---
+++ scripts/Toyyib.js
@@ -0,0 +1,18 @@
+var myDetails = {
+    surname: "Abdulganiyu",
+    lastname: "Toyyib",
+    hng7Id:  "HNG-01526",
+    language: "JavaScript"
+ };
+ 
+    
+ 
+   function fullDetails () {
+     return "Hello World, this is " + myDetails.surname + " " + myDetails.lastname + 
+     " with HNGi7 ID " + myDetails.hng7Id + " using " + myDetails.language +
+     " for stage 2 task."
+ 
+   }
+ 
+   console.log(fullDetails());
+ 
