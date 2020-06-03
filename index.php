684c1eeb5608507a3e35801d277221ce3d9ab262
David Adigwu <davidadigwu@gmail.com>
2020-06-03 01:52:18 +0100

    I added index.php file to execute scripts

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-  
+
+$names = array();
+
+$directory = "scripts/";
+
+
+foreach(glob($directory . "*.{js,php,py}", GLOB_BRACE) as $filename){
+    array_push($names,$filename);
+}
+
+
+foreach($names as $name){
+echo "<pre>$name</pre>";
+}
+
+
+foreach($names as $name){
+$output = shell_exec("php $name");
+echo "<pre>$output</pre>";
+}
+
 ?>
