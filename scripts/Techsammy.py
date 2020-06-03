d884170a10e4a275d5191118c1d61c52adfd5c76
root <root@localhost.localdomain>
2020-06-03 22:35:13 +0100

    add my script

--- scripts/Techsammy.py
+++ scripts/Techsammy.py
@@ -1,9 +1,11 @@
 #this is my python script on my project for teamfury stage2
 import json 
 name = "Aigberua Samuel"
-id = "HNG-01234"
+hng_id = "HNG-01234"
+gmail = "aigberuas7@gmail.com"
+lang = "python"
 # my python directory
 def retjson():
-	python2json = json.dumps("Hello world, this is "+name+" with HNGI7 ID: "+id+" using python for stage2 task")
+	python2json = json.dumps("Hello world, this is "+name+" with HNGI7 ID: "+hng_id+" and this is my mail "+gmail+" using "+lang+" for stage2 task")
 	print (python2json)
 retjson()
