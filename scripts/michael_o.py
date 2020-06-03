b732c1375e8b9215dc13ad4804515df318778445
omichaels <31506153+blaknight@users.noreply.github.com>
2020-06-04 06:53:05 +0800

    Update michael_o.py

--- scripts/michael_o.py
+++ scripts/michael_o.py
@@ -1,4 +1,4 @@
 def my_function():
-  print("Hello World, this is Michael N.N Oku with HNGi7 ID HNG-03221 using PYTHON for stage2 task and Email: omichaels@usa.com")
+  print("Hello World, this is Michael N.N Oku with HNGi7 ID HNG-03221 and email omichaels@usa.com using python for stage 2 task ")
 
 my_function()
