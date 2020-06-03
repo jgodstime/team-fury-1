152290126f9730f43bcd64466a0b0d96f3054bf7
Arch997 <archibongc4@gmail.com>
2020-06-03 14:27:11 +0100

    Add my script

---
+++ scripts/archibong.py
@@ -0,0 +1,13 @@
+
+class Task:
+	def __init__(self, name, id, language):
+		self.name = name
+		self.language = language
+		self.id = id
+	
+	def text(self):
+		 print(f'Hello World, this is {self.name}, with HNGi7 ID ({self.id}) using {self.language} for Stage 2 task')
+
+t = Task('Charles E.', 'HNGi7-01950', 'Python')
+t.text()
+
