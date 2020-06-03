ec2633ada093d5be052cd2eed03003b440b64758
maroafenogho <maroafenogho@users.noreply.github.com>
2020-06-03 19:52:36 +0100

    Adding Maro's Script

---
+++ scripts/Oghenemaro-Afenogho.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  String name = 'Oghenemaro Afenogho';
+  String id = 'HNG-00355';
+  String language = 'Dart';
+  String email = 'maroafenogho@gmail.com';
+
+  output(){
+    return 'Hello World, this is $name with HNGi7 ID $id using $language for stage 2 task';
+  }
+
+ print(output());
+
+}
