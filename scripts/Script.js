02401a28c6d9e6fbe321e895f76bf8b298140fbe
payn33 <amugenp2@gmail.com>
2020-06-03 01:01:46 +0100

    adds my script.js file to the scripts folder

---
+++ scripts/Script.js
@@ -0,0 +1,8 @@
+var displayText = {
+  name:"Peter Amugen",
+  id:'"HNGi-01447"',
+  language:"javascript"
+};
+
+
+document.write( "Hello world, this is " + displayText.name + " with HNGi7 ID " + displayText.id + " using " + displayText.language + " for stage 2 task.").innerHTML;
