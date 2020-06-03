50f86f4c47013914d57203dc9824936eec7a94a9
Oluwatimilehin Makinde <temak@Oluwatimilehins-MacBook-Pro.local>
2020-06-03 23:15:27 +0100

    kindly review my code

---
+++ scripts/temak-HNG-06553.js
@@ -0,0 +1,6 @@
+const name = "Oluwatimilehin Makinde";
+const id = "HNG-06553";
+const email = "makindeoluwatimilehin@gmail.com";
+let greeting = "Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using JavaScript for stage 2 task";
+
+console.log(greeting);
