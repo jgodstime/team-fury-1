49cef879c08e5e243600440f6c385b1e8fdaf03e
Abisoye <abisoyeoladayo@gmail.com>
2020-06-04 00:43:41 +0100

    abisoye script file

---
+++ scripts/abisoye.js
@@ -0,0 +1,12 @@
+let name = 'Abisoye Alli-Balogun';
+let id = 'HNG-00110';
+let language = 'Javascript';
+
+let output = "Hello World, this is " + name + " with HNGi7 " + id + " using " + language + " for stage 2 task";
+
+function displayOutput() {
+    return output;  
+};
+
+console.log(displayOutput());
+//Hello World, this is Abisoye Alli-Balogun with HNGi7 ID HNG-00110 using Javascript for stage 2 task"
