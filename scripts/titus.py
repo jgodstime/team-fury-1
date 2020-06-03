5872c5dba38c297d6c6daac430da122f48ee237d
moringa <costamay>
2020-06-03 13:50:47 +0300

    Add initial commit

---
+++ scripts/titus.py
@@ -0,0 +1,10 @@
+
+def internship():
+    fullname = "Titus Ouko"
+    myId = "HNG-00509"
+    email  = "titusouko@gmail.com"
+    language = "Python"
+    
+    return("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(fullname,  myId, email, language))
+
+print(internship())
