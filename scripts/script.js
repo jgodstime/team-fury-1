909c16ecb1bcad6d26a00d8300875ebbf09633de
Iyadi Ol'seun <42003510+CornerstoneII@users.noreply.github.com>
2020-06-04 11:27:14 +0100

    Fixed JSON

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,22 @@
-fetch('scripts/person.json')
-    .then(function (response) {
-        // The API call was successful!
-        return response.json();
-    })
-    .then(function (data) {
-        // This is the JSON from our response
-        appendData(data);
-    })
-    .catch(function (err) {
-        // If there was an error report error to console
-        console.log('error: ' + err);
-    });
-    // Assign JSON from response to a function
-function appendData(data){
-    // Iterate over the object (response from JSON)
-        for (var i = 0; i < data.length; i++) {
-            console.log(`Hello World, this is ${data[i].name} with HNGi7 ID ${data[i].id} using ${data[i].language} for stage 2 task`);
-        }
+fetch("scripts/person.json")
+  .then(function (response) {
+    // The API call was successful!
+    return response.json();
+  })
+  .then(function (data) {
+    // This is the JSON from our response
+    appendData(data);
+  })
+  .catch(function (err) {
+    // If there was an error report error to console
+    console.log("error: " + err);
+  });
+// Assign JSON from response to a function
+function appendData(data) {
+  // Iterate over the object (response from JSON)
+  for (var i = 0; i < data.length; i++) {
+    console.log(
+      `Hello World, this is ${data[i].fullname} with HNGi7 ID ${data[i].id} and email ${data[i].email} using ${data[i].language} for stage 2 task`
+    );
+  }
 }
