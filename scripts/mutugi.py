67bcb42e5bcd74a68799088a1598536994d0f01c
mutugiii <mutugimutuma@gmail.com>
2020-06-04 23:33:04 +0300

    Add mutugi.py

---
+++ scripts/mutugi.py
@@ -0,0 +1,6 @@
+name="Mutugi Mutuma"
+email="billmutuma@gmail.com"
+user_id="HNG-01414"
+langauge="Python"
+
+print(f"Hello World, this is {name} with HNGi7 ID {user_id} and email {email} using {language} for stage 2 task")
