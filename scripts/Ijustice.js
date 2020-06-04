000d4d336e7844a21f2bf432575766ebfca0277d
idrisjustice <idrisjustice21@gmail.com>
2020-06-04 19:17:37 +0100

    add my script

---
+++ scripts/Ijustice.js
@@ -0,0 +1,12 @@
+var name, hng_id, language,email;
+
+name = "Idris Justice",
+    hng_id = "HNG-01076",
+    language = "JavaScript",
+    email = "idrisjustice21@gmail.com";
+
+function showDetails()
+{
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} and email ${email} using ${language} for stage 2 task`);
+}
+showDetails();
