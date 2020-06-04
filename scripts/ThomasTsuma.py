4f8292dc1f7fa1bc77c852318c74df47b9c72470
TomiTsuma <34827918+TomiTsuma@users.noreply.github.com>
2020-06-04 15:15:57 -0700

    Update ThomasTsuma.py

--- scripts/ThomasTsuma.py
+++ scripts/ThomasTsuma.py
@@ -1,1 +1,1 @@
-﻿print("Hello World, my name is Thomas Tsuma with HNGI7 ID HNG-00332 using Python for stage 2 task 
+﻿print("Hello World, my name is Thomas Tsuma with HNGi7 ID HNG-00332 and email tommytsuma7@gmail.com using Python for stage 2 task 
