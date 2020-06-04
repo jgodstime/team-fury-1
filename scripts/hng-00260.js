74ea52977fcf1b6c0974178798fc305ce22679e5
nckappdev <thatdeveloperguy1301@gmail.com>
2020-06-04 12:39:11 +0100

    hng first task

---
+++ scripts/hng-00260.js
@@ -0,0 +1,17 @@
+
+var item = {
+    fullname: "Ajao Ataul_mujeeb",
+    id: "HNG-00260",
+    email: "ajaodlp@gmail.com",
+    language: "Javascript",
+}
+
+
+
+
+
+function start() {
+    return `Hello World, this is ${item.fullname} with ${item.id} and email ${item.email} using ${item.language} for stage 2 task`
+}
+
+start()
