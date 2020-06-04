02cc9a6067d36e1defbce88299b28e51252faba6
Mocktar <mocktarissa@gmail.com>
2020-06-04 17:25:44 +0300

    Added email

--- scripts/mocktarissa.js
+++ scripts/mocktarissa.js
@@ -2,7 +2,8 @@ const app= ()=>{
     const data={
         fullName:"Massamar Mocktar Issa",
         id:"HNG-01843",
-        language:"Javascript"
+        language:"Javascript",
+        email:"mocktarissa@gmail.com"
     } 
      console.log(`Hello World, this is ${data.fullName} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task`)
 }
