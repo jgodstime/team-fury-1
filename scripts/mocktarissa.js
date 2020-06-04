34e482ced063616e1d09718415960dd47baad9cf
Mocktar <mocktarissa@gmail.com>
2020-06-04 05:55:05 +0300

    Initial commit

---
+++ scripts/mocktarissa.js
@@ -0,0 +1,10 @@
+const app= ()=>{
+    const data={
+        fullName:"Massamar Mocktar Issa",
+        id:"HNG-01843",
+        language:"Javascript"
+    } 
+     console.log(`Hello World, this is ${data.fullName} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task`)
+}
+
+app()
