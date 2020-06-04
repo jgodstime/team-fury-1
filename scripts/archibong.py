c83418abdac14698fa1141ef165712ab0a66cfa6
Arch997 <archibongc4@gmail.com>
2020-06-04 16:49:08 +0100

    Add my script

--- scripts/archibong.py
+++ scripts/archibong.py
@@ -1,18 +1,3 @@
-import json
 
 
-class Task:
-	def __init__(self, name, id, email, language):
-		self.name = name
-		self.language = language
-		self.id = id
-		self.email = email
-	
-	def text(self):
-		 return print(f'Hello World, this is {self.name}, with HNGi7 ID {self.id} and email {self.email} using {self.language} for Stage 2 task')
-
-email = 'archibongc4@gmail.com'
-
-t = Task("Charles Effiom", 'HNGi7-01950', 'archibongc4@gmail.com', "Python")
-t.text()
-
+print("Hello World, this is Charles Effiom with HNGi7 ID HNG-01950 and email archibongc4@gmail.com using Python for stage 2 task")
