5ce1210e3e92d8ef6900904eb7fcab331190efa1
olabode-dev <muhbrahc1@gmail.com>
2020-06-04 09:17:16 +0100

    added my mail

--- scripts/Adebayo-Mubarak.js
+++ scripts/Adebayo-Mubarak.js
@@ -1,13 +1,9 @@
-//I will be using template strings
-const name = "ADEBAYO MUBARAK OLABODE"
-const id = "HNG-03225"
-const language = "Javascript"
-const email = "muhbrahc1@gmail.com"
-
-const output = `Hello World, this is ${name}, with HNGi7 ID ${id} using ${language} for stage 2 task`
-
-const introduction = () => {
-    console.log(output)
+const name = 'ADEBAYO MUBARAK OLABODE';
+const id = 'HNG-03225';
+const language = 'Javascript';
+const email = 'muhbrahc1@gmail.com';
+const output = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using Javascript for stage 2 task`;
+const Introduction = () => {
+  console.log(output);
 };
-
-introduction();
+Introduction();
