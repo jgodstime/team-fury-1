2a866956f7861025c69ba804442e6f6d00342034
Arch997 <archibongc4@gmail.com>
2020-06-04 10:27:41 +0100

    Added email

--- scripts/archibong.py
+++ scripts/archibong.py
@@ -1,12 +1,15 @@
+import json
+
 
 class Task:
 	def __init__(self, name, id, language):
 		self.name = name
 		self.language = language
 		self.id = id
+		self.email = email
 	
 	def text(self):
-		 print (f'Hello World, this is {self.name}, with HNGi7 ID {self.id} using {self.language} for Stage 2 task')
+		 return print(f'Hello World, this is {self.name}, with HNGi7 ID {self.id} and email {self.email} using {self.language} for Stage 2 task')
 
 email = 'archibongc4@gmail.com'
 
