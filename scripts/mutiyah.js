72930e61964727152783bb2d7a589fb7bd5f131d
unknown <mutiyah.kem@gmail.com>
2020-06-04 12:58:29 +0100

    mutiyah add her script for stage 2

---
+++ scripts/mutiyah.js
@@ -0,0 +1,10 @@
+var full_name = "Mutiyah Muritala";
+var hng_id = "HNG-05061";
+var language = "javaScript";
+var email = "mutiyah.kem@gmail.com";
+
+function check (output){
+    output = console.log("Hello World, this is " + full_name + " with HNGi7 ID " + hng_id + " and email " + email + " using " + language + " for stage 2 task");
+    return output;
+}
+check();
