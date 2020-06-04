3e0de01b94f237ac260f910e8581aace08ca5ced
Akinyemi <karrim4real@gmail.com>
2020-06-04 23:44:41 +0100

    add my script

--- scripts/karrim4real.php
+++ scripts/karrim4real.php
@@ -1,12 +1,11 @@
 <?php
 
   public function karrim($fullname, $email, $ID, $language){
-    echo "Hello World, This is {$fullname} with HNDi7 ID {$ID} using {$language} for the stage 2 task";
+    echo "Hello World, This is $fullname with HNDi7 ID $ID and $email using $language for the stage 2 task";
   }
 
 
    karrim("Akinyemi taiwo karrim", "karrim4real@gmail.com", "HNG-06649", "PHP");
-
 
 
  ?>
