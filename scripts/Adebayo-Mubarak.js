7230fe0be3f3ccccd6eae975307c3f2179f33341
olabode-dev <muhbrahc1@gmail.com>
2020-06-04 01:40:05 +0100

    I added my script

---
+++ scripts/Adebayo-Mubarak.js
@@ -0,0 +1,13 @@
+//I will be using template strings
+const name = "ADEBAYO MUBARAK OLABODE"
+const id = "HNG-03225"
+const language = "Javascript"
+const email = "muhbrahc1@gmail.com"
+
+const output = `Hello World, this is ${name}, with HNGi7 ID ${id} using ${language} for stage 2 task`
+
+const introduction = () => {
+    console.log(output)
+};
+
+introduction();
