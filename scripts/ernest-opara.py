17c442c083381af7bbd24b18dc969542746609fc
Ernest-Opara <ernest.ebuka@gmail.com>
2020-06-04 17:24:46 +0100

    submission for ernest-opara

---
+++ scripts/ernest-opara.py
@@ -0,0 +1,11 @@
+fullname = "Chukwuebuka Ernest-Opara"
+hng_id = "HNG-04557"
+language = "Python"
+email = "ernest.ebuka@gmail.com"
+
+def introduction():
+
+    result = ("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(fullname,hng_id,email,language))
+    print(result)
+
+introduction()
