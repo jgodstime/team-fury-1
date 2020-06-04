79286752f6e7fa3ae25b40f616dd918e15a011fe
Deifa <Deifa@users.noreply.github.com>
2020-06-05 00:01:26 +0100

    fixed script

---
+++ scripts/deifa.js
@@ -0,0 +1,11 @@
+const internDetails = {
+	fullname: "Imiebo Godson",
+	id: "HNG-04448",
+	email: "imiebo.godson@gmail.com",
+	language: "JavaScript"
+};
+
+
+const message = `Hello World, this is ${internDetails.fullname} with HNGi7 ID ${internDetails.id} using ${internDetails.language} for stage 2 task`;
+
+console.log(message);
