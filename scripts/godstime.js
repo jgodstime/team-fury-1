d8df1f8c8d268b6e06500470174f45295019037a
yediran <66297592+yediran@users.noreply.github.com>
2020-06-04 01:13:03 +0100

    Update godstime.js

---
+++ scripts/godstime.js
@@ -0,0 +1,9 @@
+{
+file: "myScript.py",
+output: "Hello World, this is Bonaventure Attado Alexander with HNGi7 ID HNG-05914 using Python for stage 2 task",
+name: "Bonaventure Attado Alexander",
+Id: "HNG-05914",
+email: "alexanderyediran@gmail.com",
+language: "Python",
+status: "Pass"
+}
