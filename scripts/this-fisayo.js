f781ab523d005dbc9e2f46b0800d149cec7ace8f
Fisayo <aikomofisayo@gmail.com>
2020-06-04 23:01:59 +0100

    Add my script

---
+++ scripts/this-fisayo.js
@@ -0,0 +1,6 @@
+const full_name = "Fisayo Aikomo";
+const hng_id = "HNG-05547";
+const language = "Javascript";
+const email = "aikomofisayo@gmail.com";
+
+console.log(`Hello World, this is ${full_name} with HNGi7 ID ${hng_id} and email ${email} using ${language} for stage 2 task`);
