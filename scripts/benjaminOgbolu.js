d71f3085ed1740245efca504309eb86f9a34d350
Benjamin-411 <61962055+Benjamin-411@users.noreply.github.com>
2020-06-04 22:13:03 +0100

    Corrected Script
    Please add the new one

--- scripts/benjaminOgbolu.js
+++ scripts/benjaminOgbolu.js
@@ -1,7 +1,7 @@
 const name = "Benjamin Ogbolu";
 const id = "HNG-02588";
 const email = "bhennie411@gmail.com";
-const language = "Javascript";
+const language = "JavaScript";
 const myScript = () => {
     return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`
 };
