4f34cdd034aa6778d3a91eb9bcf239c6cb3b0a08
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-06-04 21:43:24 +0200

    feat: add flushing

--- mocks-parser.php
+++ mocks-parser.php
@@ -1,5 +1,28 @@
 <?php
-  function parse_scripts($toJson = false, $prettify = false) {
+  $totalResult = [];
+  $passCount = 0;
+  $failCount = 0;
+  $totalCount = 0;
+  
+  $scriptsFilenames = get_scripts_filenames();
+  
+  // Returns all filenames in the specified scripts folder.
+  //
+  // In this case, we are using the mocks folder.
+  function get_scripts_filenames() {
+    $filenames = [];
+    foreach(glob("mocks/*.{js,py,php}", GLOB_BRACE) as $filename) {
+      $filenames[] = $filename;
+    }
+    return $filenames;
+  }
+  
+  // Returns a parsed script.
+  //
+  // The function accepts the filename of the script as an argument. It then uses that
+  // filename to find the file, parse it, and validate it with a pass or fail.
+  function parse_script($filename) {
+    global $totalResults, $passCount, $failCount, $totalCount;
     // scan directory for files matching pattern(s)
     // store filename in object variable
     $cli = [
@@ -9,13 +32,8 @@
     ];
     
     $regex = '/Hello World, this is ([a-zA-Z\s]+) with HNGi7 ID (HNG-\d{5}) and email ([_a-z0-9-]+[\.[_a-z0-9-]+]*@[a-z0-9-]+[\.[a-z0-9-]+]*.[a-z]{2,3}) using ([\w\s]+) for stage 2 task/i';
-    $totalResults = [];
-    $passCount = 0;
-    $failCount = 0;
-    $totalCount = 0;
-      
-    foreach (glob("mocks/*.{js,py,php}", GLOB_BRACE) as $filename) {
-      $fileExt = pathinfo($filename, PATHINFO_EXTENSION);
+    
+    $fileExt = pathinfo($filename, PATHINFO_EXTENSION);
       // echo "$fileExt , $filename \n";
       
       $result = new stdClass();
@@ -75,26 +93,39 @@
 
       $totalResults[] = $result;   
       $totalCount += 1;
-    }
 
+    return $result;
+  }
+
+  // Returns the results summary as a class.
+  function get_results_summary() {
     $summary = new stdClass();
     $summary->totalResults = $totalResults;
     $summary->passCount = $passCount;
     $summary->failCount = $failCount;
     $summary->totalCount = $totalCount;
 
-    if ($toJson) {
-      // return json
-      if($prettify){
-        $summary = json_encode($totalResults, JSON_PRETTY_PRINT);
-      }
-      else {
-        $summary = json_encode($totalResults);
-      }
-      
-    }    
-
     return $summary;
   }
-  
+
+  // Returns the results summary as in json.
+  //
+  // Can be prettified by passing true as a parameter.
+  function get_results_summary_as_json($prettify = false) {
+    global $scriptsFilenames, $totalResults;
+
+    // Run the parse script for all scripts.
+    //
+    // This will update the required data (e.g. $totalResults, $passCount, $failCount, $totalCount);
+    foreach($scriptsFilenames as $key => $filename) {
+      parse_script($filename);
+    }
+
+    if($prettify){
+      return json_encode($totalResults, JSON_PRETTY_PRINT);
+    }
+    else {
+      return json_encode($totalResults);
+    }
+  }
 ?>
