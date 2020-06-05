3f07e9454ed534e12cc025326d5641b8d03ae89d
fayeloja <fayeloja@gmail.com>
2020-06-05 14:07:58 +0100

    changed my expression from functions to step by step

--- scripts/fatai-ayeloja.php
+++ scripts/fatai-ayeloja.php
@@ -1,10 +1,11 @@
 <?php
 //Task to progress into stage 2 of HNG Internship 7.0 4-6-2020
 
+$name = "Fatai Ayeloja A.";
+$emailadd = "fayeloja@gmail.com";
+$hngi7id = "HNG-02584";
+$prolang = "PHP";
+$level = "Stage 2";
 
-function display($name, $email, $id, $prolang){
-        echo "Hello !!, My name is: $name , HNGi7 ID: $id and email add: $email using $prolang .";
-    }
-
-    display('Fatai Ayeloja A', 'fayeloja@gmail.com', 'HNG-02584', 'PHP');
+echo "Hello World, This is {$name}, with HNGi7 ID: {$hngi7id} and email address $email using $prolang for {$level} task.";
 ?>
