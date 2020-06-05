4acb2eefd6f07e1edb5dd27fd977f142dfa61212
MUNAGALA PRASHANTH REDDY <58625465+Iamprashanth-1@users.noreply.github.com>
2020-06-05 08:39:04 +0530

    Update nenu.py

--- nenu.py
+++ nenu.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-name = 'Munagala Prashanth Reddy'
+myname = 'Munagala Prashanth Reddy'
 ID = 'HNG-03383'
 email = 'mprashanth059@gmail.com'
 language = 'python'
-print(f'Hello World, this is {name} with HNGi7 ID {ID} using {language} for stage 2 task')
+print(f'Hello World, this is {myname} with HNGi7 ID {ID} using {language} for stage 2 task')
 
 
 
