25ad3c9530d96abfbf967764c82ed7c55e069681
Saviourise <65181992+Saviourise@users.noreply.github.com>
2020-06-05 15:05:31 +0100

    Update Saviee.java

--- scripts/Saviee.java
+++ scripts/Saviee.java
@@ -3,7 +3,8 @@ public class Program
     public static void main(String[] args) {
         String name = "Saviour Ise";
         String HNG_ID = "HNG-06712";
+        String email = "saviourise1@gmail.com";
         String programming_language = "Java";
-        System.out.println("Hello World, this is " + name + " with HNGi7 ID " + HNG_ID + " using " + programming_language   + " for stage 2 task");
+        System.out.println("Hello World, this is " + name + " with HNGi7 ID " + HNG_ID + " and email " + email + " using " + programming_language   + " for stage 2 task");
     }
 }
