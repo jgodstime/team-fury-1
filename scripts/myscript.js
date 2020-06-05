bef1320328967fb8bb536bfa5c985587614a3f63
nckappdev <thatdeveloperguy1301@gmail.com>
2020-06-05 10:40:35 +0100

    y scripts

---
+++ scripts/myscript.js
@@ -0,0 +1,10 @@
+const details = [
+    "Ajao Ataulmujeeb",
+    "HNG-00260",
+    "Javascript",
+    "Ajaodlp@gmail.com"
+]
+const text = (name, id, lang, email) => {
+    console.log(`Hello World, this is ${name} with HNGi7 ID  ${id} and email ${email} using ${lang} for stage 2 task`)
+};
+text(details[0], details[1], details[2], details[3]);
