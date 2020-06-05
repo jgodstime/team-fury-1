5f7b5319034720472321acfb353703b3562edee3
Reehah <44873139+Reehah@users.noreply.github.com>
2020-06-05 21:54:46 +0100

    correction

--- scripts/godstime.js
+++ scripts/godstime.js
@@ -1,6 +1,6 @@
 var name="Alade-Raji Far'hah";
 var id = "HNG-00449";
 var email = "ibiwunmi887@gmail.com"
-lang = "node.js";
+var lang = "node.js";
 
 console.log('Hello World, this is', name,'with HNGi7 ID', id,'and email',email,'using',lang,'for stage 2 task');
