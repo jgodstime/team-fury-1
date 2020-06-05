5cf1993d07ddf23164f1146c7d162f142eecb418
delight <delightiworima19@gmail.com>
2020-06-05 03:30:48 +0100

    added my script

--- delight500.js
+++ delight500.js
@@ -9,4 +9,4 @@ let email = 'delightiworima19@gmail.com'
 let myDetails = `Hello World, this is ${myname} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
  
 console.log(myDetails)
-  
+    
