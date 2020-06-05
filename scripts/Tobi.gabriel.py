687df7724cc43f8b6fdabf49262bd0c1f8afbb15
Tobi.gabriel <tobiolanipekun71@gmail.com>
2020-06-05 12:14:18 +0100

    this is my script

---
+++ scripts/Tobi.gabriel.py
@@ -0,0 +1,9 @@
+#This is my task for stage 2
+myname = "Olanipekun Tobi"
+id = "HNG-03552"
+language = "python"
+email ="tobiolanipekun71@gmail.com"
+def user():
+	print ("Hello world my name is Olanipekun Tobi with HNGI7 ID: HNG-03552 using python for stage 2 task" )
+user()
+#This will print out my name, id, using python language
