72ac957d17745c052b2e0bf56c566e82d5fca4a4
mutugiii <mutugimutuma@gmail.com>
2020-06-05 20:35:20 +0300

    Tweaks

--- scripts/mutugi.py
+++ scripts/mutugi.py
@@ -3,7 +3,4 @@ email="billmutuma@gmail.com"
 user_id="HNG-01414"
 langauge="Python"
 
-def my_function():
-    print(f"Hello World, this is {name} with HNGi7 ID {user_id} and email {email} using {language} for stage 2 task")
-
-my_function()
+print(f"Hello World, this is {name} with HNGi7 ID {user_id} and email {email} using {language} for stage 2 task")
