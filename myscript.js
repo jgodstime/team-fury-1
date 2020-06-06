a45931d085847a86ef9bccec3e30400b721f12b6
Olalekan Kolapo <okolapo@teamapt.com>
2020-06-06 09:21:12 +0100

    add script:

---
+++ myscript.js
@@ -0,0 +1,6 @@
+const fullName = "Victor Kenneth";
+const ID = "HNG-00614";
+const email = "victorkenneth95@gmail.com";
+const language = "JavaScript";
+
+console.log(`Hello World, this is ${fullName} with HNGi7-ID ${ID} and email ${email} using ${language} for stage 2 task`);
