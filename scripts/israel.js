0564d49f24381b2d6295cac1c93b06b0bd74b5fa
adeoyebrass <52764075+adeoyebrass@users.noreply.github.com>
2020-06-06 12:32:43 +0100

    new update

--- scripts/israel.js
+++ scripts/israel.js
@@ -1,11 +1,7 @@
-var text="hello world,this is ";
-var fullname="oluwafemi adeoye adeyemi ";
-var text1="with "
-var id="HNG-04195 ";
-var text2="and email "
-var email="adeoyefemi3@gmail.com ";
-var text3="using ";
-var language="javascript ";
-var stage="for stage 2 task";
+let fullname= "Oluwafemi Adeoye Adeyemi";
+let id="HNG-04195";
+let email="Adeoyefemi3@gmail.com";
+let language="javascript";
 
-console.log(text+fullname+text1+id+text2+email+text3+language+stage);
+console pass="Hello World, this is ${fullname} with HNG ID ${id} and ${email} using ${language} for stage 2 task";
+console.log(pass);
