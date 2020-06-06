04a011c1eee773281261ab5dcbd3e503f7cb54b7
Oluwasegun01 <62776131+Oluwasegun01@users.noreply.github.com>
2020-06-06 12:41:55 +0100

    Please merge my script. Corrections implemented.

--- segz01.js
+++ segz01.js
@@ -3,4 +3,4 @@ var email = "segunboye22@gmail.com";
 var hng_Id = "HNG-03864";
 var lang = "Javascript";
 
-console.log("Hello World, this is " + name +  "with ID " + hng_Id + " and email " + email + " using" + lang + " for stage 2 task.");
+console.log("Hello World, this is " + name +  " with HNGi7 ID " + hng_Id + " and email " + email + " using " + lang + " for stage 2 task");
