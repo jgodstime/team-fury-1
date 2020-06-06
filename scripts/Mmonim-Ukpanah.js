e0a0dcaac70d02e5e7d49a36f61801c59d957e3b
MmonimUkpanah <mukpanah22@gmail.com>
2020-06-06 13:04:49 +0100

    hundredth commit

---
+++ scripts/Mmonim-Ukpanah.js
@@ -0,0 +1,12 @@
+const fullName = "Mmonim Ukpanah"
+const id = "HNG-00919"
+const languageUsed= "Javascript"
+const emailAddress= "mukpanah22@gmail.com" 
+
+let helloWorld=()=>{
+console.log (`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${languageUsed} for stage 2 task. ${emailAddress}`)
+}
+
+
+
+helloWorld();
