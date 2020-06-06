6bccf01aa55439ee3bb14edcc03ee86ca41059e2
chibs4lyf <66468787+chibs4lyf@users.noreply.github.com>
2020-06-06 19:01:54 +0100

    Chibs.js
    This is my script

---
+++ scripts/Chibs.js
@@ -0,0 +1,12 @@
+let name = 'CHIBUZOR-MADUBA';
+let id = 'HNG-06509';
+let email = 'chibuzormaduba@gmail.com';
+let language = 'Javascript';
+
+let output = "Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task";
+
+function displayOutput() {
+    return output;
+}
+
+console.log(displayOutput());
