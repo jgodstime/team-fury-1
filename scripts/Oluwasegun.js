532fc73e61ab3ad1d7fec520c35b7ec5d3eba65c
Shezy02 <66261243+Shezy02@users.noreply.github.com>
2020-06-06 13:03:27 +0100

    Add files via upload

---
+++ scripts/Oluwasegun.js
@@ -0,0 +1,10 @@
+function hello () {
+
+var name="Oluwasegun";
+var id="HNG-04076";
+var email="segunmustapha520@gmail.com";
+var language="javascript";
+return(`Hello world, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
+}
+
+console.log(hello());
