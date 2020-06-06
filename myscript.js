0b0c525a91c7f5b3243536b7ccab248d6797d415
Olalekan Kolapo <Victorkenneth95@gmail.com>
2020-06-06 11:43:50 +0100

    remove hypen as requested

--- myscript.js
+++ myscript.js
@@ -3,4 +3,4 @@ const ID = "HNG-00614";
 const email = "victorkenneth95@gmail.com";
 const language = "JavaScript";
 
-console.log(`Hello World, this is ${fullName} with HNGi7-ID ${ID} and email ${email} using ${language} for stage 2 task`);
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`);
