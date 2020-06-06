5792c93ea7cf7b4009057ee647881beb68957057
Oluwasegun01 <62776131+Oluwasegun01@users.noreply.github.com>
2020-06-06 12:13:57 +0100

    Please merge.

---
+++ scripts/segz01.js
@@ -0,0 +1,6 @@
+var name = "Oluwasegun Adeboye";
+var email = "segunboye22@gmail.com";
+var hng_Id = "03864";
+var lang = "Javascript";
+
+console.log("Hello World, this is " + name +  "with ID " + hng_Id + " and email " + email + " using" + lang + " for stage 2 task.");
