8e00248dc8530ee058a07d7b9ca221eca98dc9e3
fayeloja <fayeloja@gmail.com>
2020-06-06 11:39:51 +0100

    removed email address in the statement

--- scripts/fatai-ayeloja.php
+++ scripts/fatai-ayeloja.php
@@ -2,12 +2,12 @@
 
 //Task to progress into stage 2 of HNG Internship 7.0 4-6-2020
 
-$name = "Fatai Ayeloja A.";
+$fullname = "Fatai Ayeloja";
 $emailadd = "fayeloja@gmail.com";
 $hngi7id = "HNG-02584";
-$prolang = "PHP ";
+$prolang = "PHP";
 $level = "stage 2";
 
-echo "Hello World, this is {$name} with HNGi7 ID {$hngi7id} and email {$emailadd} using $prolang for {$level} task";
+echo "Hello World, this is {$fullname} with HNGi7 ID {$hngi7id} using $prolang for {$level} task";
 
 ?>
