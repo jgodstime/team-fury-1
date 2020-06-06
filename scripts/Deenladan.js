03a246178f5c6373fd0067b0ce2e8ef5c49aaf48
Deenladan <66244949+Deenladan@users.noreply.github.com>
2020-06-06 08:19:48 +0100

    Update Deenladan.js
    Pls add this change

--- scripts/Deenladan.js
+++ scripts/Deenladan.js
@@ -1,10 +1,11 @@
-<p id="deenladan@gmail.com"></p>
+<p id="deenladan1@gmail.com"></p>
 <script>
-var fullname, HNGi7ID, language; 
+var fullname, email, hNGi7ID, language; 
             fullname = "Zaharadeen Muhammad Abubakar";
-            HNGi7ID = "HNG-05871";
+            email = deenladan1@gmail.com";
+            hNGi7ID = "HNG-05871";
             language = "JavaScript";
             
-document.getElementById("deenladan@gmail.com").innerHTML =
-"Hello World, this is "+fullname+" with HNGi7 ID "+HNGi7ID+" using "+language+" for stage 2 task";
+document.getElementById("deenladan1@gmail.com").innerHTML =
+"Hello World, this is "+fullname+" with HNGi7 ID "+hNGi7ID+" using "+language+" for stage 2 task";
 </script>
