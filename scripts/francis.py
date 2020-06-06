aca431ecae35ac5f68d8073875d3984bee6b5be0
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-06 13:12:44 +0100

    Revert "Revert "Revert "update francis.py"""

--- scripts/francis.py
+++ scripts/francis.py
@@ -1,4 +1,9 @@
-name="francis onwukwe"
+name="Francis Onwukwe"
 id = "03944"
-language = "python"
-print(f'Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task')
+language = "Python"
+
+
+def script(name,id,email,language):
+    return f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task"
+  
+print(script(name,id,language))
