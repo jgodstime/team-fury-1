abaf760674608e8c425892a921b4ccbcdef7826f
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2020-06-08 13:20:07 +0200

    Update Alick_nyirenda.js

--- scripts/Alick_nyirenda.js
+++ scripts/Alick_nyirenda.js
@@ -3,8 +3,8 @@ function getDetails() {
   let id = "HNG-05884";
   let email = "nyirenda_a@zoho.com";
   let progLanguage = "Javascript";
-  console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${ progLanguage} for task in stage 2`
-  );
+  console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${progLanguage} for stage 2 task`
+  );  
 }
 
 getDetails();
