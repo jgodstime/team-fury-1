33a3b67c0a9875f9d32a286139ca0a6965a43d8b
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2020-06-08 13:26:21 +0200

    Update Alick_nyirenda.js

--- scripts/Alick_nyirenda.js
+++ scripts/Alick_nyirenda.js
@@ -1,6 +1,6 @@
 function getDetails() {
   let fullName = "Alick Nyirenda";
-  let id = "HNG-05884";
+  let id = "HNG-05984";
   let email = "nyirenda_a@zoho.com";
   let progLanguage = "Javascript";
   console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${progLanguage} for stage 2 task`
