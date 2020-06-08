764698c394b0e0179f744a19780a7e2aeadc1e48
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2020-06-08 13:19:02 +0200

    Update and rename godstime.js to Alick_nyirenda.js

---
+++ scripts/Alick_nyirenda.js
@@ -0,0 +1,10 @@
+function getDetails() {
+  let fullName = "Alick Nyirenda";
+  let id = "HNG-05884";
+  let email = "nyirenda_a@zoho.com";
+  let progLanguage = "Javascript";
+  console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${ progLanguage} for task in stage 2`
+  );
+}
+
+getDetails();
